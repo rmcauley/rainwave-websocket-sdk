3dc91673604397cf6f809d94db5dbb6ada50967b
Rob McAuley <rmcauley@gmail.com>
2021-05-30 10:40:50 +0200

    Fix type in documentation

--- src/types/album.ts
+++ src/types/album.ts
@@ -13,7 +13,7 @@ export interface Album {
    * const artMedRes = `https://rainwave.cc/${albumArt}_240.jpg`;
    * const artMaxRes = `https://rainwave.cc/${albumArt}_320.jpg`;
    * if (!albumArt) {
-   *   const backupArt = "https://rainwave.c/static/images4/noart_1.jpg";
+   *   const backupArt = "https://rainwave.cc/static/images4/noart_1.jpg";
    * }
    * ```
    */
